fc2cfcec0453cdb5b6e1caaf9ff27639a712e534
ctroupin <ctroupin@ulg.ac.be>
2017-06-08 14:39:45 +0200

    pep8

--- examples/divand_argo.py
+++ examples/divand_argo.py
@@ -8,23 +8,23 @@ fname = "diva_bath.nc"
 
 nc = netCDF4.Dataset(fname)
 
-b = nc.variables["bat"][:,:]
+b = nc.variables["bat"][:, :]
 lon = nc.variables["lon"][:]
 lat = nc.variables["lat"][:]
 
-Lon, Lat = np.meshgrid(lon,lat)
+Lon, Lat = np.meshgrid(lon, lat)
 mask = b < 0
 
-pm, pn = metric(Lon,Lat)
+pm, pn = metric(Lon, Lat)
 
 # data file
 A = np.loadtxt("temperature_argo.txt")
 
-xobs = A[:,0];
-yobs = A[:,1];
-vobs = A[:,2];
-lenx = 800e3; # in meters
-leny = 800e3; # in meters
+xobs = A[:, 0]
+yobs = A[:, 1]
+vobs = A[:, 2]
+lenx = 800e3    # in meters
+leny = 800e3    # in meters
 epsilon2 = 1.
 
 # compute anomalies
@@ -32,8 +32,9 @@ vmean = np.mean(vobs[:])
 vanom = vobs - vmean
 
 # make the analysis
-va, s = divand(mask, (pm, pn), (Lon, Lat), (xobs, yobs), vobs, (lenx, leny), epsilon2);
+va, s = divand(mask, (pm, pn), (Lon, Lat), (xobs, yobs), vobs, (lenx, leny), epsilon2)
 
 v = va + vmean
-plt.pcolor(Lon,Lat,v); plt.colorbar()
+plt.pcolor(Lon, Lat, v)
+plt.colorbar()
 plt.savefig("divand_argo.png")
